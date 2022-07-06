7d092a769646029c82dd16c24299adf9d450562e
Марат <mtomarov@edu.hse.ru>
2022-07-06 20:43:06 +0300

    Added new functions

--- yatube/manage.py
+++ yatube/manage.py
@@ -8,45 +8,69 @@ class Record:
             self.date = dt.datetime.now().date()
         else:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
-        self.USD_RATE = 55
-        self.EUR_RATE = 60
+
 
 class Calculator:
     def __init__(self, limit):
         self.records = []
         self.limit = limit
-
-class CashCalculator(Calculator):
-    #def __init__(self, limit):
-        #super().__init__(self, limit)
-
+    
     def add_record(self, record):
         self.records.append(record)
-    
+
     def get_today_stats(self):
         spending = 0
         for i in self.records:
             if i.date == dt.datetime.now().date(): spending += i.amount
         return spending
+
+
+class CashCalculator(Calculator):
+    def __init__(self, limit):
+        super().__init__(limit)
+        self.USD_RATE = 55
+        self.EUR_RATE = 60
     
     def get_today_cash_remained(self, currency):
-        spending = self.get_today_stats()
-        if (self.limit - spending > 0):
-            print(f"На сегодня осталось {self.limit - spending} {currency}\n")
-        elif (self.limit - spending == 0):
-            print(f"Денег нет, держись\n")
+        left = (self.limit - self.get_today_stats())
+        if currency == 'eur':
+            left /= self.EUR_RATE
+        elif currency == 'usd':
+            left /= self.USD_RATE
+        
+        if (left > 0):
+            return (f'На сегодня осталось {left} {currency}')
+        elif (self.left == 0):
+            return (f'Денег нет, держись')
         else:
-            print(f"Денег нет, держись: твой долг – {spending - self.limit} {currency}\n")
+            return (f'Денег нет, держись: твой долг – {-left} {currency}')
+    
+    def get_week_stats(self):
+        spending = 0
+        for i in self.records:
+            if i.date >= (dt.datetime.now().date() - dt.timedelta(weeks = 7))  and i.date <= dt.datetime.now().date(): 
+                spending += i.amount
+        return spending
+
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        left = (self.limit - self.get_today_stats())
+        if (left > 0):
+            return (f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {left} кКал\n')
+        else:
+            return (f'Хватит есть!\n')
+
 
 cash_calculator = CashCalculator(1000)
 
-cash_calculator.add_record(Record(amount=145, comment="кофе")) 
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
+cash_calculator.add_record(Record(amount=145, comment='кофе')) 
+cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+cash_calculator.add_record(Record(amount=3000, comment='бар в Танин др', date='08.11.2019'))
                 
+print(cash_calculator.get_today_cash_remained("eur"))
 print(cash_calculator.get_today_cash_remained("rub"))
+print(cash_calculator.get_week_stats())
 
 
 
