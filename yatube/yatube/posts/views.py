0e05d0b0ef13f194de7fc210afdf9bd4b6b83ceb
Марат <mtomarov@edu.hse.ru>
2022-07-12 01:13:06 +0300

    created base template of server and added bootstrap design extension

--- yatube/yatube/posts/views.py
+++ yatube/yatube/posts/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 from .models import Post
 
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:10]
+    """
     output = []
     for item in latest:
         output.append(item.text)
     return HttpResponse('\n'.join(output))
+    """
+    return render(request, "index.html", {"posts":latest})
     
 
 # Create your views here.
